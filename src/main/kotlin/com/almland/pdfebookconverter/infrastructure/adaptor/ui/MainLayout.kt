81f6b486b8120d4121795491b18f45a93bb23e7d
Alex Morland <morlandalex@googlemail.com>
2024-11-11 09:54:42 +0100

    do not blocking ui by the download and processing

--- src/main/kotlin/com/almland/pdfebookconverter/infrastructure/adaptor/ui/MainLayout.kt
+++ src/main/kotlin/com/almland/pdfebookconverter/infrastructure/adaptor/ui/MainLayout.kt
@@ -1,15 +1,19 @@
 package com.almland.pdfebookconverter.infrastructure.adaptor.ui
 
 import com.vaadin.flow.component.applayout.AppLayout
-import com.vaadin.flow.component.applayout.DrawerToggle
 import com.vaadin.flow.component.html.H1
+import com.vaadin.flow.dom.Style.Position.ABSOLUTE
 
 internal class MainLayout : AppLayout() {
 
     init {
         addToNavbar(
-            DrawerToggle(),
-            H1("T converter")
+            H1("T converter").apply {
+                style
+                    .setFontSize("var(--lumo-font-size-l)")
+                    .setLeft("var(--lumo-space-l)")
+                    .setPosition(ABSOLUTE)
+            }
         )
     }
 }
--- src/main/kotlin/com/almland/pdfebookconverter/infrastructure/adaptor/ui/view/ContentView.kt
+++ src/main/kotlin/com/almland/pdfebookconverter/infrastructure/adaptor/ui/view/ContentView.kt
@@ -6,16 +6,19 @@ import com.vaadin.flow.component.Component
 import com.vaadin.flow.component.Composite
 import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.combobox.ComboBox
+import com.vaadin.flow.component.combobox.ComboBoxVariant
 import com.vaadin.flow.component.html.Anchor
 import com.vaadin.flow.component.icon.VaadinIcon.DOWNLOAD
 import com.vaadin.flow.component.orderedlayout.FlexComponent
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.component.progressbar.ProgressBar
 import com.vaadin.flow.component.upload.Upload
 import com.vaadin.flow.component.upload.receivers.MemoryBuffer
-import com.vaadin.flow.dom.Style
 import com.vaadin.flow.router.Route
 import com.vaadin.flow.server.InputStreamFactory
 import com.vaadin.flow.server.StreamResource
+import java.io.InputStream
+import kotlin.concurrent.thread
 
 @Route(value = ContentView.PATH, layout = MainLayout::class)
 internal class ContentView(private val aggregateQueryPort: AggregateQueryPort) : Composite<Component>() {
@@ -27,44 +30,77 @@ internal class ContentView(private val aggregateQueryPort: AggregateQueryPort) :
 
     private lateinit var upload: Upload
     private lateinit var anchor: Anchor
-    private lateinit var target: ComboBox<FileTarget>
+    private lateinit var layout: VerticalLayout
+    private lateinit var progressBar: ProgressBar
+    private lateinit var comboBox: ComboBox<FileTarget>
 
     override fun initContent(): Component =
         VerticalLayout().apply {
             setSizeFull()
-            style.setFlexWrap(Style.FlexWrap.WRAP)
             justifyContentMode = FlexComponent.JustifyContentMode.CENTER
             defaultHorizontalComponentAlignment = FlexComponent.Alignment.CENTER
 
-            target = ComboBox<FileTarget>().apply {
-                placeholder = "convert pdf to"
-                setItems(FileTarget.entries)
-                addValueChangeListener { upload.isVisible = true }
+            val form = VerticalLayout().apply {
+                layout = this
+                width = "30%"
+                minWidth = "20em"
+
+                ComboBox<FileTarget>().apply {
+                    comboBox = this
+                    setWidthFull()
+                    placeholder = "convert T to"
+                    setItems(FileTarget.entries)
+                    addValueChangeListener { upload.isVisible = true }
+                    addThemeVariants(ComboBoxVariant.LUMO_ALIGN_CENTER)
+                }
+                MemoryBuffer().let { memory ->
+                    Upload(memory).apply {
+                        upload = this
+                        maxFiles = 1
+                        setWidthFull()
+                        isVisible = false
+                        setAcceptedFileTypes(*ACCEPTED_FILE_TYPES)
+                        addFileRemovedListener { anchor.removeFromParent() }
+                        addSucceededListener { createDownloadProgressBar(); createDownloadLink(memory) }
+                    }
+                }
+
+                add(comboBox, upload)
             }
-            upload = MemoryBuffer().let { memory ->
-                Upload(memory).apply {
-                    maxFiles = 1
-                    isVisible = false
-                    setAcceptedFileTypes(*ACCEPTED_FILE_TYPES)
-                    addFileRemovedListener { anchor.removeFromParent() }
-                    addSucceededListener { add(createAnchor(target.value.target, memory)) }
+
+            add(form)
+        }
+
+    private fun createDownloadProgressBar() {
+        ProgressBar().apply {
+            layout.add(this)
+            setWidthFull()
+            progressBar = this
+            isIndeterminate = true
+        }
+    }
+
+    private fun createDownloadLink(memory: MemoryBuffer) {
+        ui.ifPresent {
+            thread {
+                val inputStream = aggregateQueryPort.create(comboBox.value.target, memory.inputStream)
+                it.access {
+                    layout.add(createAnchor(inputStream, comboBox.value.target, memory))
+                    progressBar.removeFromParent()
                 }
             }
-
-            add(target, upload)
         }
+    }
 
-    private fun createAnchor(target: String, memory: MemoryBuffer): Anchor =
-        Anchor(createStreamResource(target, memory), null).apply {
+    private fun createAnchor(inputStream: InputStream, target: String, memory: MemoryBuffer): Anchor =
+        Anchor(createStreamResource(inputStream, target, memory), null).apply {
             anchor = this
-            add(Button("Download converted", DOWNLOAD.create()))
+            setWidthFull()
+            add(Button("Download converted", DOWNLOAD.create()).apply { setWidthFull() })
         }
 
-    private fun createStreamResource(target: String, memory: MemoryBuffer): StreamResource =
-        StreamResource(
-            getFileName(target, memory),
-            InputStreamFactory { aggregateQueryPort.create(target, memory.inputStream) }
-        )
+    private fun createStreamResource(inputStream: InputStream, target: String, memory: MemoryBuffer): StreamResource =
+        StreamResource(getFileName(target, memory), InputStreamFactory { inputStream })
 
     private fun getFileName(target: String, memory: MemoryBuffer): String =
         "${memory.fileName.substringBeforeLast(".")}.$target"
